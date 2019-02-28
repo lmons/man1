52e58d5cbc1797af28f00744831e0b3baa6578a7
unknown <mounssif.hamoumi@thinline.ma>
2019-02-28 20:57:24 +0000

    first commit

---
+++ custom/Extension/application/Ext/EntryPointRegistry/customEntryPoint.php
@@ -0,0 +1,4 @@
+<?php
+$entry_point_registry['getCyclePlanPeriod'] = array('file' => 'custom/modules/Thin_CyclePlans/getCyclePlanPeriod.php',    'auth' => true);
+$entry_point_registry['getKeyMessages'] = array('file' => 'custom/modules/Thin_Key_Messages/getKeyMessages.php',    'auth' => true);
+$entry_point_registry['getNonVisitedTarget'] = array('file' => 'modules/Bugs/Dashlets/MyBugsDashlet/datatables.php',    'auth' => true);
---
+++ custom/application/Ext/EntryPointRegistry/entry_point_registry.ext.php
@@ -0,0 +1,9 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+
+
+$entry_point_registry['getCyclePlanPeriod'] = array('file' => 'custom/modules/Thin_CyclePlans/getCyclePlanPeriod.php',    'auth' => true);
+$entry_point_registry['getKeyMessages'] = array('file' => 'custom/modules/Thin_Key_Messages/getKeyMessages.php',    'auth' => true);
+$entry_point_registry['getNonVisitedTarget'] = array('file' => 'modules/Bugs/Dashlets/MyBugsDashlet/datatables.php',    'auth' => true);
+
+?>
